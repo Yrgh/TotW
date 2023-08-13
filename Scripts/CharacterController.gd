34966b2008bfc36bc521d32e253e816e357e0abd
Yrgh <141231102+Yrgh@users.noreply.github.com>
2023-08-13 15:16:56 -0400

    Add files via upload

--- Scripts/CharacterController.gd
+++ Scripts/CharacterController.gd
@@ -1,19 +1,48 @@
 extends CharacterBody3D
 
+#Constants
+const BASE_SPEED = 1.5
+const SPRINT_SPEED = 2.5
+#When drowsy OR out of stamina
+const TIRED_SPEED = 0.75
 
-const BASE_SPEED = 3.0
-const SPRINT_SPEED = 4.5
-const TIRED_SPEED = 1.5
-const JUMP_SPEED_BONUS = 5.0
-const JUMP_VELOCITY = 3.75
+const JUMP_SPEED_BONUS = 3.0
+const JUMP_VELOCITY = 1.5
+
+#Although the character is already above-average,
+#we multiply their attributes to make the player
+#feel more powerful and not slow
+const POWER_MULTIPLIER = 3.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-@onready var neck := $Neck
-@onready var head := $Neck/Camera3D
+@onready var head := $'Eyes/Camera SpringArm'
+@onready var body := $Guy
+@onready var eyes := $Eyes
+@onready var container := $'..'
 
-func _unhandled_input(event):
+func diamond(v: Vector2):
+	if v.x != 0 && v.y != 0:
+		return .5*v
+	else:
+		return v
+
+func fix_alignment() -> void:
+	var id = diamond(Input.get_vector("left", "right", "forward", "backward"))
+	var wasd = id.x != 0 || id.y != 0
+	if head.spring_length <= .6 || (wasd&&is_on_floor()):
+		rotate_y(eyes.rotation.y)
+		eyes.rotation.y = 0
+
+func clamp_cam() -> void:
+	if head.spring_length <= .11:
+		head.rotation.x = clamp(head.rotation.x,deg_to_rad(-60),deg_to_rad(90))
+	else:
+		head.rotation.x = clamp(head.rotation.x,deg_to_rad(-75),deg_to_rad(10))
+	
+
+func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	elif event.is_action_pressed("ui_cancel"):
@@ -21,20 +50,39 @@ func _unhandled_input(event):
 	
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
-			neck.rotate_y(-event.relative.x*.004)
+			eyes.rotate_y(-event.relative.x*.004)
 			head.rotate_x(-event.relative.y*.004)
-			head.rotation.x = clamp(head.rotation.x,deg_to_rad(-60),deg_to_rad(90))
-	
+			clamp_cam()
+			fix_alignment()
 
-func _physics_process(delta):
+func f_to_v3(f: float) -> Vector3:
+	return Vector3(f,f,f)
+
+func get_horiz_speed(v: Vector3) -> float:
+	return Vector2(v.x,v.z).length()
+
+const walkMult = PI*POWER_MULTIPLIER
+func _physics_process(delta: float) -> void:
+	clamp_cam()
+	fix_alignment()
+	
+	head.spring_length = Global.animCD.upd(delta,Global.cameraDist)
+	
+	if Input.is_action_just_pressed('zoom_in'):
+		Global.cameraDist -= .5 + Global.cameraDist * .1
+	if Input.is_action_just_pressed('zoom_out'):
+		Global.cameraDist += .5 + Global.cameraDist * .1
+	
+	Global.cameraDist = clamp(Global.cameraDist,.1,7.5)
+	
+	if head.spring_length <= .6:
+		body.visible = false
+	else:
+		body.visible = true
+	
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
-	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y).normalized())
-	var wasd = false
-	if input_dir.x != 0 || input_dir.y != 0:
-		Global.walkTime += delta
-		wasd = true
-	if !wasd || !is_on_floor():
-		Global.walkTime = 0
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var wasd = input_dir.x != 0 || input_dir.y != 0
 	
 	var speed
 	if Global.staminaOut:
@@ -46,6 +94,8 @@ func _physics_process(delta):
 		speed = SPRINT_SPEED
 		Global.stamina -= 24 * delta
 	
+	var wallClimb := is_on_wall() && Input.is_action_pressed("jump") && !Global.staminaOut
+	
 	if is_on_floor():
 		velocity.x *= 0.5
 		velocity.z *= 0.5
@@ -53,32 +103,56 @@ func _physics_process(delta):
 			Global.stamina += 7 * delta
 		else:
 			Global.stamina += 21 * delta
-	else:
-		velocity *= 0.975
+	elif !(wallClimb || Global.climbLastFrame):
 		velocity.y -= gravity * delta
-
+	else:
+		velocity = f_to_v3(0)
+		Global.stamina -= 4 * delta
+		if Input.is_action_pressed("forward"):
+			velocity.y = 1
+			Global.stamina -= 10 * delta
+		elif Input.is_action_pressed("backward"):
+			velocity.y = -1.1
+			Global.stamina -= 7 * delta
+	
 	if Input.is_action_just_pressed("jump") && is_on_floor() && Global.stamina > 0 && !Global.staminaOut:
-		velocity.y = JUMP_VELOCITY
+		velocity.y = JUMP_VELOCITY  * POWER_MULTIPLIER
 		speed += JUMP_SPEED_BONUS
-		Global.stamina -= 7
+		Global.stamina -= 7 
 		
 	Global.stamina = clamp(Global.stamina,-1,100)
 	
-	if is_on_floor():
+	#Actual movement V
+	if is_on_floor() || (Global.climbLastFrame && !wallClimb):
 		if !wasd:
 			velocity.x *= .5
 			velocity.z *= .5
 		if direction:
-			velocity.x += direction.x * speed
-			velocity.z += direction.z * speed
+			velocity.x += direction.x * speed * POWER_MULTIPLIER * .5
+			velocity.z += direction.z * speed * POWER_MULTIPLIER * .5
+			#.5 is for balancing
 		else:
 			velocity.x = move_toward(velocity.x, 0, speed)
 			velocity.z = move_toward(velocity.z, 0, speed)
+	elif !wallClimb:
+		velocity *= f_to_v3(.99)
 	
 	if transform.origin.y <= -10.0:
-		transform.origin = Vector3(0,0,0)
-		velocity = Vector3(0,0,0)
+		transform.origin = f_to_v3(0)
+		velocity = f_to_v3(0)
+		Global.reset()
 	
-	head.transform.origin = Vector3(0,.05*sin(4*speed*Global.walkTime),0)
-
+	Global.climbLastFrame = Input.is_action_pressed("jump") && !Global.staminaOut && !is_on_floor() && (wallClimb || Global.climbLastFrame)
+	
+	if wasd && is_on_floor():
+		Global.walkTime += delta
+	else:
+		Global.walkTime = 0
+	
+	if head.spring_length <= .6:
+		var inp = Global.walkTime*walkMult*speed
+		eyes.position.y = .05*sin(inp) + .02*cos(inp*.5)
+		eyes.rotation.z = .01*sin(inp*.5)
+		eyes.position.x = .02*cos(inp*.5)
+	
 	move_and_slide()
--- Scripts/globals.gd
+++ Scripts/globals.gd
@@ -3,14 +3,56 @@ extends Node
 var stamina = 100.0
 var staminaOut = false
 
-var walkTime = 0
+const CD_F = 1.0
+const CD_ZETA = 0.8
+const CD_R = 1.5
+
+var cameraDist = .1
+var animCD = BetterAnimation.new(CD_F,CD_ZETA,CD_R,0.1)
+
+var climbLastFrame := false
 
 @onready var ScreenSize = get_window().get_size()
 
 var StaminaBarPad
 @onready var StaminaBarSize
 
+var walkTime = 0
+
 func update_vars():
 	ScreenSize = get_window().get_size()
 	StaminaBarSize = Vector2(3*ScreenSize.x/30,ScreenSize.x/30)
 	StaminaBarPad = Vector2(ScreenSize.x/175,ScreenSize.x/175)
+	
+func reset():
+	climbLastFrame = false
+	staminaOut = false
+	stamina = 100.0
+	walkTime = 0
+
+class BetterAnimation:
+	var xp : float
+	var y : float
+	var yd : float
+	
+	var k1 : float
+	var k2 : float
+	var k3 : float
+	
+	func _init(f: float,z: float, r: float, x: float):
+		k1 = z/(PI*f)
+		k2 = 1/((2*PI*f)**2)
+		k3 = r * z/(2*PI*f)
+		
+		xp = x
+		y = x
+		yd = 0
+	
+	func upd(delta: float, x: float, xd = null):
+		if xd == null:
+			xd = (x-xp)/delta
+			xp = x
+		y += delta * yd
+		yd += delta * (x+ k3*xd - y - k1*yd)/k2
+		return y
+
